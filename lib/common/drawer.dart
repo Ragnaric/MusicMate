d1c6d917c935b9c5118d4254b82f8e65ca55a8d0
Jonathan Navarrete <navakino@gmail.com>
2024-11-17 21:21:19 -0700

    Add sections to custom drawer widget

--- lib/common/drawer.dart
+++ lib/common/drawer.dart
@@ -9,6 +9,26 @@ class CDrawer extends StatelessWidget {
     return SafeArea(
       child: Drawer(
         width: ScreenUtils.screenWidth(context) * .85,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              SizedBox(height: CSizes.defaultSpace),
+              Text('Metronome'),
+              SizedBox(height: CSizes.defaultSpace),
+              Text('Tuner'),
+              SizedBox(height: CSizes.defaultSpace),
+              Text('Sheet Music'),
+              SizedBox(height: CSizes.defaultSpace),
+              Text('Preferences'),
+              SizedBox(height: CSizes.defaultSpace),
+              Text('Rate this app'),
+              SizedBox(height: CSizes.defaultSpace),
+              Text('Share this app'),
+              SizedBox(height: CSizes.defaultSpace),
+            ],
+          ),
+        ),
       )
     );
   }
